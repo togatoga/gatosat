5f1d335c583333fec46c9a1f7a7f206ea570727f
togatoga <togasakitogatoga@gmail.com>
2018-02-13 22:52:36 +0900

    Update lit.go

--- lit.go
+++ lit.go
@@ -2,6 +2,8 @@ package main
 
 type Var int
 
+const VarUndef Var = -1
+
 type Lit struct {
 	X int
 }
@@ -16,6 +18,27 @@ func NewLit(x Var, sign bool) Lit {
 	return p
 }
 
+func (l *Lit) Equal(p Lit) bool {
+	if l.X != p.X {
+		return false
+	}
+	return true
+}
+
+func (l *Lit) NotEqual(p Lit) bool {
+	if l.X == p.X {
+		return false
+	}
+	return true
+}
+
+func (l *Lit) Less(p Lit) bool {
+	if l.X > p.X {
+		return false
+	}
+	return true
+}
+
 func (l *Lit) Sign() bool {
 	if l.X&1 == 0 {
 		return false
@@ -25,4 +48,8 @@ func (l *Lit) Sign() bool {
 
 func (l *Lit) Var() Var {
 	return Var(l.X >> 1)
+}
+
+func (l *Lit) ToInt() int {
+	return l.X
 }
