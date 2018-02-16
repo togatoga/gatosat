728acd451d737b46fc2c7b2c519ea834811a8127
togatoga <togasakitogatoga@gmail.com>
2018-02-16 16:31:19 +0900

    Add clause.go

--- clause.go
+++ clause.go
@@ -1,4 +1,29 @@
 package main
 
+type Header struct {
+	Mark     uint
+	Learnt   bool
+	HasExtra bool
+	Size     int
+}
+
 type Clause struct {
+	header Header
+	Data   []Lit
+	Act    float32
+}
+
+func NewClause(ps []Lit, useExtra, learnt bool) Clause {
+	var c Clause
+	c.header.Mark = 0
+	c.header.Learnt = learnt
+	c.header.HasExtra = useExtra
+	c.header.Size = len(ps)
+
+	for i := 0; i < len(ps); i++ {
+		c.Data = append(c.Data, ps[i])
+	}
+	c.Act = 0
+
+	return c
 }
