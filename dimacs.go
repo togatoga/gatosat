ec9ecd016124c74ac33bbd0b5504f0ab9f1ab432
togatoga <togasakitogatoga@gmail.com>
2018-02-19 19:00:02 +0900

    Update solver.go

---
+++ dimacs.go
@@ -0,0 +1,5 @@
+package main
+
+func readClause(s *Solver, lits []Lit) {
+
+}
--- solver.go
+++ solver.go
@@ -1,4 +1,13 @@
 package main
 
 type Solver struct {
+	Verbosity bool
+}
+
+func (s *Solver) addClause(lits []Lit) {
+
+}
+
+func (s *Solver) Solve() {
+
 }
