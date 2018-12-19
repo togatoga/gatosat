9a1362657aee65456e7a7d138eee50190e487524
togatoga <togasakitogatoga@gmail.com>
2018-12-19 11:04:14 +0900

    Impl TestSolve

--- solver_test.go
+++ solver_test.go
@@ -1,7 +1,46 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
 
 func TestSolve(t *testing.T) {
+	//SAT
+	satDir := "test/sat"
+	satFiles, err := ioutil.ReadDir(satDir)
+	if err != nil {
+		panic(err)
+	}
+	for _, satFile := range satFiles {
+		if satFile.IsDir() {
+			continue
+		}
+		if strings.HasSuffix(satFile.Name(), ".cnf") {
+			fileName := filepath.Join(satDir, satFile.Name())
 
+			f, err := os.Open(fileName)
+			if err != nil {
+				panic(err)
+			}
+			buf := bufio.NewScanner(f)
+			fmt.Println("The solver is solving a sat problem... ", fileName)
+			solver := NewSolver()
+			err = parseDimacs(buf, solver)
+			if err != nil {
+				fmt.Println(err, fileName)
+				continue
+			}
+			status := solver.Solve()
+			if status != LitBoolTrue {
+				err = fmt.Errorf("The solver returns a wrong value for a sat problem: %s", fileName)
+				panic(err)
+			}
+		}
+	}
 }
