342c0d61a8d08321f673cf076609ff61fb5651e6
togatoga <togasakitogatoga@gmail.com>
2018-12-21 08:29:45 +0900

    Add comments to watcher.go

--- watcher.go
+++ watcher.go
@@ -1,10 +1,12 @@
 package main
 
+//Watcher is the struct to detect conflicts
 type Watcher struct {
-	claRef  ClauseReference
-	blocker Lit
+	claRef  ClauseReference //claRef is a reference for a clause
+	blocker Lit             //blocker is a checker variable whether a clause is conflicted or not
 }
 
+//NewWatcher returns a pointer of Watcher
 func NewWatcher(cla ClauseReference, p Lit) *Watcher {
 	return &Watcher{
 		claRef:  cla,
@@ -12,6 +14,7 @@ func NewWatcher(cla ClauseReference, p Lit) *Watcher {
 	}
 }
 
+//Equal returns a boolean indicating a clause reference is equal
 func (w *Watcher) Equal(wr Watcher) bool {
 	if w.claRef == wr.claRef {
 		return true
@@ -19,14 +22,17 @@ func (w *Watcher) Equal(wr Watcher) bool {
 	return false
 }
 
+//Watches is a struct for watchers
 type Watches struct {
 	watches [][]*Watcher
 }
 
+//NewWatches returns a pointer of Watches
 func NewWatches() *Watches {
 	return &Watches{}
 }
 
+//Init append a new empty watcher if the size of watches is greater than a variable
 func (w *Watches) Init(v Var) {
 	size := 2*int(v) + 1
 	for len(w.watches) <= size {
@@ -34,16 +40,19 @@ func (w *Watches) Init(v Var) {
 	}
 }
 
+//Lookup returns a pointer of literal's watches
 func (w *Watches) Lookup(x Lit) *[]*Watcher {
 	idx := LitToInt(x)
 	return &(w.watches[idx])
 }
 
+//Append appends a new watcher to watches
 func (w *Watches) Append(x Lit, watcher *Watcher) {
 	idx := LitToInt(x)
 	w.watches[idx] = append(w.watches[idx], watcher)
 }
 
+//RemoveWatcher removes a watcher which has literal x from watches
 func RemoveWatcher(watches *Watches, x Lit, watcher *Watcher) {
 	startCopyIdx := -1
 	//Find the index of watcher
